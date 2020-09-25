65c240da2b675b891a1d21de924871c1b5a36373
JCinelli <37580071+JCinelli@users.noreply.github.com>
2020-09-25 13:09:19 +0200

    @CrossOrigin

--- src/main/java/dev/collegue/repository/CollegueRepository.java
+++ src/main/java/dev/collegue/repository/CollegueRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import dev.collegue.entity.Collegue;
 
+@CrossOrigin
 public interface CollegueRepository extends JpaRepository<Collegue, Integer> {
 
 	List<Optional<Collegue>> findByNom(String nom);
